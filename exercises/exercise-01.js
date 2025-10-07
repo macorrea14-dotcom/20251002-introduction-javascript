b2f4dbb9bc9887e043e06358c36f0be3041ba264
Mateo Correa Pino <ma.correa14@ciaf.edu.co>
2025-10-07 11:29:23 -0500

    agrege el ejercicio-02

---
+++ exercises/exercise-01.js
@@ -0,0 +1,4 @@
+let nombre = "Mateo"
+let hobbie = "Video Juegos"
+
+console.log( "Hola mi nombre es" `${nombre}` + "y me gustan los" + `${hobbie}`);
---
+++ exercises/exercise-02.js
@@ -0,0 +1,7 @@
+const num1 = 5.5;
+const num2 = 10.5;
+
+let Suma = num1 + num2
+let Multiplicación = num1 * num2
+
+console.log(Suma + Multiplicación);
